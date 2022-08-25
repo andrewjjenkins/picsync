6b5c3b1b0c3f919e4e69d032d2d5450c1ae0cb86
Andrew Jenkins <andrewjjenkins@gmail.com>
2022-08-25 11:52:03 -0600

    Report token expiry as "valid time remaining" instead
    Problem: Reporting token expiry as a float64 is awkward to work with.
    Plotters like grafana would have to convert it into a timestamp and diff
    it from their local time which may produce confusing results and is easy
    to get wrong.
    
    Solution: Report how long we, the server, think is valid in remaining
    token lifetime.  This will be easier to work with - as long as this is a
    positive number, then everything is good.  In reality, probably expect
    it to be more than 60 (as in, valid for more than one more minute).  If
    it ever becomes negative, you should expect errors fetching google
    photos.

--- pkg/googlephotos/prom.go
+++ pkg/googlephotos/prom.go
@@ -72,8 +72,8 @@ func (c *clientImpl) promRegister(reg prometheus.Registerer) error {
 		return err
 	}
 	c.prom.tokenExpiry = c.prom.promFactory.NewGaugeFunc(prometheus.GaugeOpts{
-		Name: "googlephotos_access_token_expiry",
-		Help: "Time that the current access token expires (unix timestamp)",
+		Name: "googlephotos_access_token_valid_time_remaining",
+		Help: "Number of seconds the access token is valid for (negative if expired)",
 	}, expiryGetter)
 	return nil
 }
@@ -89,22 +89,23 @@ func (c *clientImpl) newTokenExpiryGetter() (func() float64, error) {
 	tokenFetcher := func() float64 {
 		mu.Lock()
 		defer mu.Unlock()
-		since := time.Now().Sub(lastTokenFetchTime)
+		since := time.Since(lastTokenFetchTime)
+		now := time.Now()
 		if since < 5*time.Minute {
 			// return cached expiry time to avoid triggering too many refreshes
 			// just for stats.  Unlikely to happen in the case that refreshes
 			// are actually successful, but if refreshes fail, this limits how
 			// often we try to refresh.
-			return float64(lastToken.Expiry.Unix())
+			return float64(lastToken.Expiry.Sub(now).Milliseconds() / 1000.0)
 		}
 		t, err := c.tokenSource.Token()
 		if err != nil {
 			fmt.Printf("Warning: failed to get a token: %s\n", err.Error())
-			return float64(lastToken.Expiry.Unix())
+			return float64(lastToken.Expiry.Sub(now).Milliseconds() / 1000.0)
 		}
 		lastToken = t
-		lastTokenFetchTime = time.Now()
-		return float64(lastToken.Expiry.Unix())
+		lastTokenFetchTime = now
+		return float64(lastToken.Expiry.Sub(now).Milliseconds() / 1000.0)
 	}
 	return tokenFetcher, nil
 }
